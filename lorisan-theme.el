5023dbf770bf330c6c5f9c7e402449f93500be18
hubisan <hubisan@gmail.com>
2018-07-02 23:51:13 +0200

    ready for melpa

--- lorisan-theme.el
+++ lorisan-theme.el
@@ -1,34 +1,28 @@
-;;; lorisan-theme.el --- Lorisan color theme for Emacs > 24
+;;; lorisan-theme.el --- Dark theme;;; lorisan-theme.el --- Lorisan color theme for Emacs > 24
 
-;; Copyright 2015 Daniel Hubmann
+;; Copyright 2015-2018 Daniel Hubmann
 
 ;; Author: Daniel Hubmann <hubisan@gmail.com>
-;; Version: 1.082
 ;; URL: https://github.com/hubisan/lorisan-theme
-;; Keywords: lorisan, dark, themes
+;; Version: 1.1.0
+;; Package-Requires: ((emacs "24"))
 
-;; This file is not part of GNU Emacs
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
-;; This file is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published
-;; by the Free Software Foundation, either version 3 of the License,
-;; or (at your option) any later version.
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
 
-;; This file is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-;; General Public License for more details.
-;; <http://www.gnu.org/licenses/>.
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
-;; Dark color theme for emacs with intensive colors.
-;; The theme structure has been partly borrowed from grandshell theme
-;; by steckerhalter
-
-;;; Requirements:
-
-;; Emacs 24.
+;; Lorisan, dark color theme
 
 ;;; Code:
 
@@ -1074,10 +1068,10 @@
   (add-to-list 'custom-theme-load-path
                (file-name-as-directory (file-name-directory load-file-name))))
 
+(provide-theme 'lorisan)
+
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:
-
-(provide-theme 'lorisan);
 
 ;;; lorisan-theme.el ends here
